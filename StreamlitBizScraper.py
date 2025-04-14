26d0b91e09f8f9d801e4cc21f17e9f3a4467b5ac
CyrusCorrell0 <cyruscorrell07@gmail.com>
2025-04-14 15:06:32 -0700

    Update StreamlitBizScraper.py

--- StreamlitBizScraper.py
+++ StreamlitBizScraper.py
@@ -9,6 +9,7 @@ from io import StringIO
 st.title("Business Phone Number Scraper")
 
 # Input for bounding box coordinates
+st.write("Default boundary box set to SF, map visualization coming soon!")
 st.header("Bounding Box Coordinates")
 col1, col2 = st.columns(2)
 with col1:
